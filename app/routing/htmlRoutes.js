bf0374ab2ab5428a2ed7139a63df2353efe42012
An Phan <antphan98@gmail.com>
2019-12-20 23:49:23 -0600

    modified routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,15 +2,15 @@ const path = require("path");
 
 module.exports = function(app) {
 
-    app.get("/", function(req, res) {
+    app.get("/home", function(req, res) {
 
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "/../public/home.html"));
 
     });
 
-    app.get("/", function(req, res) {
+    app.use(function(req, res) {
 
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
 
     });
 
