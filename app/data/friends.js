706de8239a59a9f4518f0bc9a21c921de61fccec
An Phan <antphan98@gmail.com>
2019-12-20 22:13:53 -0600

    added all of friends into an array

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,7 @@ const friends =
     [{
 
         "name": "Pikachu",
-        "photo": "",
+        "photo": "https://66.media.tumblr.com/c8ffc8959e3a922fcee91f086d5914ef/tumblr_pecewd4uUU1xde1l9o3_250.jpg",
         "scores": [
             3,
             1,
@@ -19,7 +19,7 @@ const friends =
         ]
     }, {
         "name": "Charmander",
-        "photo": "",
+        "photo": "https://pbs.twimg.com/media/D7RkELXXkAA9Xwa.jpg",
         "scores": [
 
             4,
@@ -38,7 +38,7 @@ const friends =
     }, {
 
         "name": "Bulbasaur",
-        "photo": "",
+        "photo": "https://pbs.twimg.com/media/D7RkBZ9WkAAn6ih?format=jpg&name=4096x4096",
         "scores": [
 
             2,
@@ -57,7 +57,7 @@ const friends =
     }, {
 
         "name": "Squirtle",
-        "photo": "",
+        "photo": "https://pbs.twimg.com/media/D7RkFs5XYAAJvTj?format=jpg&name=small",
         "scores": [
 
             1,
@@ -77,7 +77,7 @@ const friends =
     }, {
 
         "name": "Snorlax",
-        "photo": "",
+        "photo": "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/89ef6a6a-8dfb-4765-8e3d-0fdec4581c63/d5hkj45-5630b62f-bb34-4630-a871-77316546ef5e.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzg5ZWY2YTZhLThkZmItNDc2NS04ZTNkLTBmZGVjNDU4MWM2M1wvZDVoa2o0NS01NjMwYjYyZi1iYjM0LTQ2MzAtYTg3MS03NzMxNjU0NmVmNWUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.p9O0cYJkXaKxZrVBxWBmkMSmELZeXmt1LltM_xZIdPs",
         "scores": [
 
             5,
@@ -94,3 +94,5 @@ const friends =
         ]
 
     }]
+
+    module.exports = friends;
